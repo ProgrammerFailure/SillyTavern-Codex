573f84b853cb9d951018e75164ca8456479bd057
LenAnderson <Anderson.Len@outlook.com>
2024-04-14 08:22:56 -0400

    fix book loading

--- src/st/wi/Settings.js
+++ src/st/wi/Settings.js
@@ -69,15 +69,15 @@ export class WorldInfoSettings {
             ...(world_info.globalSelect ?? []),
             chat_metadata.world_info,
             characters[context.characterId]?.data?.character_book?.name,
-            ...world_info.charLore?.find(it=>it.name == characters[context.characterId]?.avatar?.split('.')?.slice(0,-1)?.join('.'))?.map(it=>it.extraBooks) ?? [],
+            ...(world_info.charLore?.find(it=>it.name == characters[context.characterId]?.avatar?.split('.')?.slice(0,-1)?.join('.'))?.map(it=>it.extraBooks) ?? []),
             ...(groups
                 .find(it=>it.id == context.groupId)
                 ?.members
                 ?.map(m=>[
                     ...(characters.find(it=>it.avatar == m)?.data?.character_book?.name ?? []),
                     ...(world_info.charLore?.find(it=>it.name == m.split('.').slice(0,-1).join('.'))?.extraBooks ?? []),
-                ])
-                    ?? []
+                ])?.flat()
+                ?? []
             ),
         ].filter(it=>it);
         names = names.filter((it,idx)=>names.indexOf(it) == idx).toSorted((a,b)=>a.localeCompare(b));
