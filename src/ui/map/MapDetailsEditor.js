48ff1a35aec314608c20c5a730bb2ad7417e391d
LenAnderson <Anderson.Len@outlook.com>
2024-05-28 18:31:13 -0400

    add image browser to maps

--- src/ui/map/MapDetailsEditor.js
+++ src/ui/map/MapDetailsEditor.js
@@ -1,4 +1,5 @@
 import { callPopup, getRequestHeaders } from '../../../../../../../script.js';
+import { POPUP_TYPE, Popup } from '../../../../../../popup.js';
 import { quickReplyApi } from '../../../../../quick-reply/index.js';
 import { imgUpload } from '../../lib/imgUpload.js';
 
@@ -63,6 +64,47 @@ export class MapDetailsEditor {
                 url.disabled = false;
                 url.dispatchEvent(new Event('input', { bubbles:true }));
             }
+        });
+        /**@type {HTMLElement} */
+        const urlBrowse = dom.querySelector('#stcdx--map-url-browse');
+        urlBrowse.addEventListener('click', async()=>{
+            const browseDom = document.createElement('div'); {
+                browseDom.classList.add('stcdx--explorer');
+                browseDom.textContent = 'Loading...';
+            }
+            fetch('/api/plugins/files/list', {
+                method: 'POST',
+                headers: getRequestHeaders(),
+                body: JSON.stringify({
+                    folder: '~/user/images/codex',
+                }),
+            }).then(async(response)=>{
+                if (!response.ok) {
+                    alert('Something went wrong');
+                    return;
+                }
+                const files = (await response.json()).filter(it=>it.type == 'file' && ['jpg', 'jpeg', 'png', 'webp', 'bmp', 'gif'].includes(it.path.split('.').pop()));
+                browseDom.innerHTML = '';
+                for (const file of files) {
+                    const img = document.createElement('img'); {
+                        img.classList.add('stcdx--explorer-thumb');
+                        img.src = `/user/images/codex/${file.path}`;
+                        img.addEventListener('click', ()=>{
+                            url.value = `/user/images/codex/${file.path}`;
+                            url.dispatchEvent(new Event('input', { bubbles:true }));
+                            dlg.completeAffirmative();
+                        });
+                        browseDom.append(img);
+                    }
+                }
+            });
+            const dlg = new Popup(browseDom, POPUP_TYPE.TEXT, null, {
+                okButton:'Cancel',
+                wide: true,
+                large: true,
+            });
+            dlg.dom.style.zIndex = '10010';
+            await dlg.show();
         });
         /**@type {HTMLTextAreaElement}*/
         const description = dom.querySelector('#stcdx--map-description');
