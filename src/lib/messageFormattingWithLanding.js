f762b1cbb70bbf75a5b726464fe17d9f974978d4
LenAnderson <Anderson.Len@outlook.com>
2024-06-19 09:13:14 -0400

    add stripping for "custom-" class prefix

--- src/lib/messageFormattingWithLanding.js
+++ src/lib/messageFormattingWithLanding.js
@@ -1,13 +1,14 @@
 import { messageFormatting, setCharacterId, this_chid } from '../../../../../../script.js';
 import { selected_group } from '../../../../../group-chats.js';
 
-export const messageFormattingWithLanding = (messageText)=>{
+export const messageFormattingWithLanding = (messageText, stripCustom = false)=>{
     const currentChatId = this_chid;
     let landingHack = false;
     if ((this_chid ?? selected_group) == null) {
         landingHack = true;
         setCharacterId(1);
     }
+
     messageText = messageFormatting(
         messageText,
         'Codex',
@@ -15,6 +16,10 @@ export const messageFormattingWithLanding = (messageText)=>{
         false,
         null,
     );
+    if (stripCustom) {
+        messageText = messageText.replace(/(custom-)+stcdx--/g, 'stcdx--');
+    }
+
     if (landingHack) {
         setCharacterId(currentChatId);
     }
